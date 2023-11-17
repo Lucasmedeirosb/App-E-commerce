d462f814037380e7188eeb0a34a16561a839165a
Lucasmedeirosb <103223285+Lucasmedeirosb@users.noreply.github.com>
2023-11-17 05:57:49 -0300

    Add buttons

---
+++ App.tsx
@@ -0,0 +1,18 @@
+
+import { Box, NativeBaseProvider } from 'native-base';
+import { Home } from './src/page/Home';
+
+import {theme} from './src/styles/theme'
+
+export default function App() {
+  
+  
+  return (
+    <NativeBaseProvider theme={theme}>
+      <Box flex="1" bg={theme.colors.primary[100]}>
+      <Home />
+      </Box>
+    </NativeBaseProvider>
+  );
+}
+
